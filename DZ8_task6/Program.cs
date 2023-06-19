110ce344cf70ac5bf722d72ced49d352ad22357f
Timur Karimov <tationkar@gmail.com>
2023-06-20 01:09:26 +0300

    Pascal

---
+++ DZ8_task6/Program.cs
@@ -0,0 +1,41 @@
+﻿// Показать треугольник Паскаля
+using System;
+using static System.Console;
+Clear();
+Write("Введите количество строк треугольника Паскаля: ");
+int rowTriangle = int.Parse(ReadLine());
+int[][] pascalInt = new int[rowTriangle][];
+for (int i = 0; i < rowTriangle; i++)
+{
+    pascalInt[i] = new int[(i + 1)];
+}
+pascalInt[0][0] = 1;
+pascalInt[1][0] = 1;
+pascalInt[1][1] = 1;
+for (int i = 2; i < rowTriangle; i++)
+{
+    pascalInt[i][0] = 1;
+    pascalInt[i][i] = 1;
+    for (int j = 1; j < i; j++)
+    {
+        pascalInt[i][j] = pascalInt[i - 1][j - 1] + pascalInt[i - 1][j];
+    }
+}
+PrintArray(pascalInt);
+void PrintArray(int[][] inArray)
+{
+    string centerText = string.Empty;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < (i + 1); j++)
+        {
+            centerText +=" " + $"{inArray[i][j]}" + " ";
+
+        }
+        int centerX = (Console.WindowWidth / 2) - (centerText.Length / 2);
+        Console.SetCursorPosition(centerX, i+1);
+        WriteLine(centerText);
+        centerText = string.Empty;
+    }
+    WriteLine();
+}
